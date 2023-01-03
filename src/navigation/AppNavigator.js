6d5b19bd634f9f360edda253ddf9d1ca53613e62
prabhu1652002 <basavaprabhu1652002@gmail.com>
2023-01-03 23:05:36 +0530

    refactored navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import NewsDetailsScreen from '../screens/NewsDetailsScreen';
 import NewsListScreen from '../screens/NewsListScreen';
@@ -12,14 +12,29 @@ import {MaterialIcons} from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Tabs = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
+// const Drawer = createDrawerNavigator();
 
-
+const HeaderRight = () => {                 // for info button on right side of header of any screen
+    const navigation = useNavigation();
+    return(
+        <MaterialIcons name = "info " size={24} onPress={() => (
+            navigation.navigate('AboutScreen')
+        )}></MaterialIcons>
+    )
+}
 function HomeNavigator(){
     return (
-        <Stack.Navigator screenOptions={{headerShown: true}}>
+        <Stack.Navigator screenOptions={{headerRight: () => <HeaderRight/>}}>
                 <Stack.Screen name="NewsList" component={NewsListScreen} options={{title:'All News'}} />
                 <Stack.Screen name="NewsDetails" component={NewsDetailsScreen}/>
+                <Stack.Screen name="AboutScreen" component={AboutScreen}/>
+        </Stack.Navigator>
+    )
+}
+function FavoriteNavigator(){
+    return (
+        <Stack.Navigator screenOptions={{headerRight: () => <HeaderRight/>}}>
+                <Stack.Screen name="Favorite" component={FavoriteScreen} />
         </Stack.Navigator>
     )
 }
@@ -42,16 +57,39 @@ function TabsNavigator(){
             }
         })}
         >
-            <Tabs.Screen name = "Home" component={HomeNavigator} options={{headerShown: false}} />
-            <Tabs.Screen name = "Favorites" component={FavoriteScreen} />
+            <Tabs.Screen name = "Home" component={HomeNavigator} options={{headerShown: false}}/>
+            <Tabs.Screen name = "Favorites" component={FavoriteNavigator} />
         </Tabs.Navigator>
     )
 }
-
+// main navigtion system, tab-> stack
 function AppNavigator(){
     return (
         <NavigationContainer>
-        
+            <Tabs.Navigator 
+        screenOptions={ 
+            ({route})=>({
+            
+            tabBarIcon: () => {
+                let iconName;
+                if(route.name=='Home'){
+                    iconName="home"
+                }else if(route.name=='Favorites'){
+                    iconName="favorite"
+                }
+                return(
+                    <MaterialIcons name={iconName} size={24}></MaterialIcons>
+                )
+            }
+        })}
+        >
+            <Tabs.Screen name = "Home" component={HomeNavigator} options={{headerShown: false}} />
+            <Tabs.Screen name = "Favorites" component={FavoriteScreen} />
+        </Tabs.Navigator>
+            {/* <Drawer.Navigator>
+                <Drawer.Screen name="News" component={TabsNavigator}></Drawer.Screen>
+                <Drawer.Screen name="About" component={AboutScreen}></Drawer.Screen>
+            </Drawer.Navigator> */}
         </NavigationContainer>
     )
 }
